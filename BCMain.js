0d023e313005b1110533b857d8b6349017009bb2
kurtzcarson <cek2@williams.edu>
2019-12-23 23:07:59 -0700

    Finished basic implementation... proof of work later

--- BCMain.js
+++ BCMain.js
@@ -1,6 +1,6 @@
 
 // very common cryptographic hashing technique developed by the NSA
-const SHA256 = require('crypto-js/sha256.js');
+const SHA256 = require('crypto-js/sha256');
 
 // designed to represent our blocks in the blockchain
 class Block {
@@ -16,22 +16,21 @@ class Block {
     this.timeStamp = timeStamp;
     this.data = data;
     this.previousHash = previousHash;
-
-    //holds the hash of our block
-    this.hash = calculatehash();
+    this.hash = this.calculateHash();
   }
 
   //can consider building my own hashing function instead of library
-  calculatehash() {
+  calculateHash() {
     //checks data of block and creates unique hashCode for identification
-    return SHA256( this.index + this.previousHash + this.timeStamp + JSON.stringify(this.data).toString() );
+    return SHA256( this.index + this.previousHash + this.timeStamp + JSON.stringify(this.data)).toString();
   }
+}
 
-  class BLockChain {
+  class BlockChain {
 
     constructor() {
       //first block on chain is called genesis block and initialized manually
-      this.chain = [createGenesisBlock]; // consider making this a LinkedList
+      this.chain = [this.createGenesisBlock()]; // consider making this a LinkedList
     }
 
     createGenesisBlock() {
@@ -39,7 +38,7 @@ class Block {
     }
 
     getLatestBlock() {
-      return this.chain[this.chain.length-1]
+      return this.chain[this.chain.length - 1]
     }
 
     addBlock(newBlock) {
@@ -49,5 +48,47 @@ class Block {
       newBlock.hash = newBlock.calculateHash();
       this.chain.push(newBlock);
     }
+
+    isChainValid() {
+      //return true if chain is valid
+
+      //checking if blocks are properly linked together
+      for (let i = 1; i < this.chain.length; i++) {
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i-1];
+
+        if (currentBlock.hash != currentBlock.calculateHash()) {
+          return false; //actual hash is not accurate
+        }
+        if (currentBlock.previousHash != previousBlock.hash) {
+          return false; // does not point to the proper block
+        }
+      }
+
+      return true;
+
+    }
   }
-}
+
+  let myChain = new BlockChain();
+
+  //data can be anything I want
+  myChain.addBlock( new Block(1, "12/23/2019", {amount: 4}));
+  myChain.addBlock( new Block(2, "12/24 /2019", {amount: 10}) );
+
+  console.log('Is blockchain valid? ' + myChain.isChainValid());
+
+  //never delete or change a block to keep the blockChain valid/true
+  myChain.chain[1].data = { amount: 100 };
+  myChain.chain[1].hash = myChain.chain[1].calculateHash(); // the relationship with previousBlock now broken
+
+  console.log('Is blockchain valid? ' + myChain.isChainValid());
+
+  console.log(JSON.stringify(myChain, null, 4));
+
+  /*
+  *
+  * Things to implement: block breaks chain need to 'rollback changes' and put blockchain back into original state
+  * Proof of work/ peer to peer network/ need to check if have funds to make a transaction
+  *
+  */
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Practicing Blockchain implementation 
+Learning more about the functionality of block chain throough implementation of basic blockchain structure 
