ac2f9c02c5737bd351f36d47eef2bf6e17fcdfb6
kurtzcarson <cek2@williams.edu>
2019-12-31 23:49:39 -0700

    Documentation - Need to look at bug

--- BlockChain.js
+++ BlockChain.js
@@ -2,42 +2,41 @@
 // very common cryptographic hashing technique developed by the NSA
 const SHA256 = require('crypto-js/sha256');
 
-// utilizing elliptic curves for encrypting data to save space for comparable security
+// utilizing elliptic curves for encrypting data to save space with comparable security
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
 // designed to represent our blocks in the blockchain
 class Block {
 
-  // index: optional / where the block sits on chain
-  // timeStamp: when block was created
-  //transaction data: any data associated with block (details of transactions)
-  //previousHash: string that contains hash of block before
-
-
   constructor(timeStamp, transactions, previousHash = '') {
-    this.timeStamp = timeStamp;
-    this.transactions = transactions;
-    this.previousHash = previousHash;
+    this.timeStamp = timeStamp; //when the block was created
+    this.transactions = transactions; // array of transactions
+    this.previousHash = previousHash; // hash value of previous block in chain
     this.hash = this.calculateHash();
     this.nonce = 0; //should make this random later
   }
 
-  //can consider building my own hashing function instead of library
   calculateHash() {
-    //checks data of block and creates unique hashCode for identification
+    //pre: given data of block
+    //post: returns a hash value as a string
+
+    //checks data of block and creates unique hashCode for identification (takes in a string)
     return SHA256( this.previousHash + this.timeStamp + JSON.stringify(this.transactions) + this.nonce).toString();
+
   }
 
   mineBlock(difficulty) {
-    //proof of work or mining/ set difficulty so steady amount of new blocks/ no overflow or hacking
+    // pre: integer representing the difficulting with higher integers being more difficult
+    //post: makes time to mine block (proof of work) more laborious
 
-    //guarentees all hash values are below some target hash...keeps calculating until finds a nonce that derives small hash based on unique data
+    //continue changing nonce until zeros on front of hash meet number of zeroes of difficulty
     while (this.hash.substring(0, difficulty) != new Array(difficulty + 1).join("0")) {
       this.hash = this.calculateHash();
-      this.nonce++; // avoids infinite while loop
+      this.nonce++; //arbitrary number added to data to change hash value
     }
 
+    //mainly for debugging purposes
     console.log("Block mined: " + this.hash);
 
   }
@@ -55,45 +54,58 @@ class Block {
   }
 }
 
+  //chain of blocks with array as an underlying implemenation (consider SLL where next references hash value)
   class BlockChain {
 
     constructor() {
-      //first block on chain is called genesis block and initialized manually
-      this.chain = [this.createGenesisBlock()]; // consider making this a LinkedList
 
-      //helps control howcleard fast new blocks can be added to the blockchain
+      //first block on chain is called genesis block and initialized manually
+      this.chain = [this.createGenesisBlock()];
+
+      //number of leading zeroes hash needs to contain when finding hash
       this.difficulty = 2;
 
       //an array that holds all transactions that have yet to be processed as previous blocks are being mined
       this.pendingTransactions = [];
+
+      //reward given to miners that mine blocks
       this.miningReward = 50;
     }
 
     createGenesisBlock() {
-      return new Block("12,23/19", "Genesis Block", "0");
+
+      //creates a block with trivial data to start the blockchain
+      return new Block( Date.now(), "Genesis Block (No Transactions) ");
     }
 
     getLatestBlock() {
+
+      //grabs the most recent block of the blockchain
       return this.chain[this.chain.length - 1]
     }
 
     minePendingTransactions(miningRewardAddress) {
-      //if person with mining address successfully finds hash first, then send mining reward to them
+      //pre: public key of the miner
+      //post: miner given cryptocurrency award by the network
 
-      //does not have a from address/ algorithm just gives it to you
+      //adds the reward as a transaction to the miners address
       this.pendingTransactions.push( new Transaction( null, miningRewardAddress, this.miningReward ) )
+      /**** Reconsider ordering of this transaction when having multiple people mining transaction ****/
 
-      let block = new Block( Date.now(), this.pendingTransactions )
-      block.mineBlock(this.difficulty);
-
+      //creates a new block with array of pending transactions as the data of the block
+      let newBlock = new Block( Date.now(), this.pendingTransactions )
+      newBlock.mineBlock(this.difficulty);
       console.log("Block successfully mined.");
-      this.chain.push(block);
 
-      this.pendingTransactions = [];
+      //adds newly mined block to the chain
+      this.addBlock(newblock);
+
 
     }
 
     addTransaction(transaction) {
+      //pre: non-empty, existent, valid transaction
+      //post: adds the transaction to the transactinons array
 
       //what if transaction does not have a fromAddress bc being paid?
       if(!transaction.fromAddress || !transaction.toAddress ) {
@@ -105,20 +117,24 @@ class Block {
         throw new Error('Cannot pass invalid transaction to the chain');
       }
 
-      //still need to verify that the fromAddress has high enough balance to guarentee transaction
+      //veryify that person has enough money to pay transaction
+      if (this.getBalanceOfAddress(transaction.fromAddress) < transaction.amount) {
+        throw new Error('Not enough money in account for transaction.');
+      }
+
       this.pendingTransactions.push(transaction);
 
     }
 
     getBalanceOfAddress(address) {
-      //go through entire blockChain and find all blocks associated with your address to find your balance
+      //pre: public key
+      //post: number of 'coins' associated with public key
 
       let balance = 0;
 
-      //should consider improving this run time/ each person has calculated balance
+      //iterates through each block and corresponding transactions to find transactions involving 'address'
       for (const block of this.chain) {
         for (const transaction of block.transactions) {
-
           if (transaction.toAddress == address) {
             balance += transaction.amount;
           }
@@ -126,20 +142,23 @@ class Block {
             balance -= transaction.amount;
           }
         }
+
+        //balance associated with the transaction
       } return balance;
     }
 
 
     addBlock(newBlock) {
-      //really should have more checks in place before adding to the chain
+      //pre: given a mined block
+      //post: adds the block to the blockchain
 
       newBlock.previousHash = this.getLatestBlock().hash;
-      newBlock.mineBlock(this.difficulty);
       this.chain.push(newBlock);
+      this.pendingTransactions = [];
     }
 
     isChainValid() {
-      //return true if chain is valid
+      //post: returns true if chain is valid
 
       //checking if blocks are properly linked together
       for (let i = 1; i < this.chain.length; i++) {
@@ -174,13 +193,19 @@ class Block {
     }
 
     signTransaction(signingKey) {
+      //pre: signingKey pair of private and public values for block on chain
+      //post: create verifiable signiture tied to transaction
+
+      //signingKey is object received from elliptic library i.e ec.genKeyPair();
 
       if(signingKey.getPublic('hex') != this.fromAddress) {
         throw new Error("You cannot sign transaction for other wallets");
       }
 
-      //signingKey is object received from elliptic library i.e ec.genKeyPair();
+      //calculates hash of transaction (unique with data and public key of sender)
       const hashTrans = this.calculateHash();
+
+      //creates a signiture with the hash of the transaction
       const signiture = signingKey.sign(hashTrans, 'base64');
       this.signiture = signiture.toDER('hex');
 
@@ -188,7 +213,7 @@ class Block {
 
     isValid() {
 
-      //if payment for mining a block with no from address
+      //if payment for mining a block with no from address or genesis block
       if (this.fromAddress == null) return true;
 
       //if there is no signiture or is empty
@@ -196,10 +221,24 @@ class Block {
         throw new Error('No signiture in this transaction.')
       }
 
-      //if there is a signiture, but need to verify it's correct
+      //public key of the specified address
       const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+
+      //verifies the hash of the transaction equals the signiture with the publicKey being the sender
       return publicKey.verify(this.calculateHash(), this.signiture);
 
+    }
+
+    getToAddress() {
+      return this.toAddress;
+    }
+
+    getFromAddress() {
+      return this.fromAddress;
+    }
+
+    getSigniture() {
+      return this.signiture;
     }
 }
 
--- README.md
+++ README.md
@@ -5,4 +5,7 @@ What I want to continue to explore...
 - 2.) Implementing the chain as a linked list, instead of an array...
 - 3.) Read more about other hashing functions other than SHA256 (maybe those I learn about in my cryptography research)
 - 4.) Letting miners choose which transactions to process/mine into a block from pending transactions
-- 5.) Ensure given address has funds to make transaction
+- 5.) Having a specified number of transactions that can be put into a single block
+- 6.) Implementing multiple miners at once
+- 7.) Optimize implementation finding balance of public key
+- 8.) Making more checks before adding the block to the chain
